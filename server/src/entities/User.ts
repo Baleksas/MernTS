1ad889e97c239ebd1b5cad0b105371316b9194d3
Baleksas <alexasbag17@gmail.com>
2022-01-04 16:53:13 +0000

    feat: Register resolver, hashing password with argon2

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -2,4 +2,23 @@ import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 import { ObjectType, Field } from "type-graphql";
 @ObjectType()
 @Entity()
-export class User {}
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: "date", default: "NOW()" })
+  createdAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
+  // Field exposes
+  @Field()
+  @Property({ type: "text", unique: true })
+  username!: string;
+
+  @Property({ type: "text" })
+  password!: string;
+}
